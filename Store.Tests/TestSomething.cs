7601cac25c99f3126adbaf952b7039df19d8bfee
Le Ngoc Cuong <lengoccuong02042004@gmail.com>
2024-09-28 18:28:08 +0700

    Cập nhật UI 1 phần giỏ hàng

---
+++ Store.Tests/TestSomething.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Store_EF.Models;
+using Store_EF.Models.Extensions;
+
+namespace Store.Tests
+{
+    [TestClass]
+    public class TestSomething
+    {
+        [TestMethod]
+        public void FormattedPrice() { 
+            Product product = new Product() {
+                ProductId = 1,
+                Title = "Laptop",
+                Price = 100000,
+                Stock = 1,
+                Description = "Test",
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+            Console.WriteLine(product.FormattedPrice());
+            Console.WriteLine(product.FormattedPrice(quantity: 5));
+        }
+    }
+}
